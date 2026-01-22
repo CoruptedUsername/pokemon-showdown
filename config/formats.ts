092fc6ecabde175c09155e8b605b451259fecb71
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-01-22 14:53:45 -0600

    Cottonee Cove Server v0.0.15 - Setting up Three Musketeers

--- config/formats.ts
+++ config/formats.ts
@@ -120,7 +120,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 	},
 	{
 		name: "[Gen 9] Three Musketeers Draft Week 3",
-		mod: "gen9threemusks",
+		mod: "gen93m",
 		gameType: 'doubles',
 		ruleset: ['Standard Doubles', 'NatDex Mod', 'Item Clause = 1', 'Adjust Level = 50', 'Picked Team Size = 4', 'VGC Timer', 'Terastal Clause', 'Best of = 3', '!Evasion Moves Clause', '!Gravity Sleep Clause', 'Evasion Clause', 'DryPass Clause'],
 		banlist: ['Vanilla', 'Last Respects', 'Moody', 'Quick Claw', 'King\'s Rock', 'Razor Fang', 'Shed Tail'],

cc3308eaa905211a51d6a7255423074a20734771
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-02-09 22:56:51 -0600

    Cottonee Cove Server v0.1.0 Beta 1.4 - Other Formats Re-Enabled

--- data/mods/gen9natu/scripts.ts
+++ data/mods/gen9natu/scripts.ts
@@ -1,5 +1,15 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
+	teambuilders: [
+		{
+			tierType: "tier",
+			builderTable: "gen9natu",
+			tierNames: ["gen9natalieused"],
+			tierSlices: ["NatU"],
+			isNatDex: true,
+			validTiers: ["NatU", "NatU NFE", "NatU LC"],
+		},
+	],
 	side: {
 		inherit: true,
 		canDynamaxNow() {

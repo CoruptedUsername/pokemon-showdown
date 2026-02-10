0ce830c9419c59582f43b799758c9cb47700e98b
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-02-09 21:08:09 -0600

    Cottonee Cove Server v0.1.0 Beta 1.2 - Adding validTiers variable

--- data/mods/gen9dnu/scripts.ts
+++ data/mods/gen9dnu/scripts.ts
@@ -6,6 +6,7 @@ export const Scripts: ModdedBattleScriptsData = {
 		tierNames: ["gen9donotuse", "gen9donotuseuu", "gen9donotuseru", "gen9donotuseubers", "gen9donotuseag"],
 		tierSlices: ["DoNU OU", "DoNU UU", "DoNU RU", "DoNU Uber", "DoNU AG"],
 		isNatDex: true,
+		validTiers: ["DoNU AG", "DoNU Uber", "DoNU OU", "DoNU UUBL", "DoNU UU", "DoNU RUBL", "DoNU RU"],
 	},
 	teambuilder1: {
 		tierType: "doublesTier",
@@ -13,5 +14,6 @@ export const Scripts: ModdedBattleScriptsData = {
 		tierNames: ["gen9dnuvgc"],
 		tierSlices: ["DoNU Restricted"],
 		isNatDex: true,
+		validTiers: ["DoNU Restricted", "DoNU Unrestricted"],
 	},
 };

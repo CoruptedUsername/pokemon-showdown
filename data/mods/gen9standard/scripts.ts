9aeaab3301a9da1c397c4edec359247e33b33a89
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-02-11 23:28:37 -0600

    Cottonee Cove Server v0.1.0 Beta 1.7 - Bug fixes

---
+++ data/mods/gen9standard/scripts.ts
@@ -0,0 +1,17 @@
+export const Scripts: ModdedBattleScriptsData = {
+	gen: 9,
+	teambuilders: {
+		"gen9standard": {
+			tierType: "tier",
+			formats: {
+				"gen9standard": {
+					topSlice: "OU",
+					defaultLevel: 100,
+				},
+			},
+			formatType: "singles",
+			isNatDex: false,
+			validTiers: ["OU", "UUBL", "UU", "RUBL", "RU", "NUBL", "NU", "PUBL", "PU", "ZUBL", "ZU", "NFE", "LC"],
+		},
+	},
+};

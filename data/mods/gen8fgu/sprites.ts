b82c281ed5ac94e7e274b03514ad7e1fea7b4359
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-04-01 14:52:07 -0500

    Cottonee Cove Server v0.2.0 Pre-Release 3 - Initial Teambuilder Sprite Testing

--- data/mods/gen8fgu/sprites.ts
+++ data/mods/gen8fgu/sprites.ts
@@ -1,5 +1,5 @@
 export const Sprites: import('../../../sim/dex-species').ModdedSpeciesSprites = {
-	"groudon": {
-		stuff: "theam",
+	"ivysaurrb": {
+		stuff: "./gen8fgu/sprites/ivysaurrb/ivysaurrbteambuilder.png",
 	},
 };

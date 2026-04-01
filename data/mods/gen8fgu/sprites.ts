5ab241affe80d5d8b6ca029822f63cc0682b65a1
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-04-01 18:16:33 -0500

    Cottonee Cove Server v0.2.0 Pre-Release 3.9 - Attempting teambuilder sprite overriding

--- data/mods/gen8fgu/sprites.ts
+++ data/mods/gen8fgu/sprites.ts
@@ -1,6 +1,6 @@
 export const Sprites: import('../../../sim/dex-species').ModdedSpeciesSprites = {
-	"ivysaurrb": {
-		spriteDirectory: "gen8fgu/sprites/ivysaurrb",
+	ivysaurrb: {
+		spriteDirectory: "ivysaurrb",
 		teambuilderSprite: "img.png",
 	},
 	beedrillrb: {

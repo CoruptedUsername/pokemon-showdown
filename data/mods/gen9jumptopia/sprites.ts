8aa47fbd962d83fc6f07661116e2305a46cea39a
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-04-25 13:46:03 -0500

    Cottonee Cove Server v0.2.3e - Sprite Format Change test

---
+++ data/mods/gen9jumptopia/sprites.ts
@@ -0,0 +1,9 @@
+export const Sprites: import('../../../sim/dex-species').ModdedSpeciesSprites = {
+	frozalmon: {
+		customSprite: {
+			spriteDirectory: "/.sprites/frozalmon/",
+			teambuilderSprite: "frozalmonSprite.png",
+			frontSprite: "frozalmonSprite.png",
+		},
+	},
+};

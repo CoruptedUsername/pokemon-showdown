a35e5e6eece125912e834b5db9b902f7a3f34e31
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-09-25 21:33:24 -0500

    Cottonee Cove Server v0.2.8.4.14 - Fixing coruptedblast

--- data/mods/gen93mdls2w2/moves.ts
+++ data/mods/gen93mdls2w2/moves.ts
@@ -9,10 +9,10 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		priority: 0,
 		flags: { protect: 1, mirror: 1, metronome: 1, mustpressure: 1 },
 		onPrepareHit(target, source, move) {
-			this.attrLastMove('[anim] Tera Blast ' + source.types[source.types.length() - 1]);
+			this.attrLastMove('[anim] Tera Blast ' + source.types[source.types.length - 1]);
 		},
 		onModifyType(move, pokemon, target) {
-			move.type = pokemon.types[pokemon.types.length() - 1];
+			move.type = pokemon.types[pokemon.types.length - 1];
 		},
 		onModifyMove(move, pokemon) {
 			if (pokemon.getStat('atk', false, true) > pokemon.getStat('spa', false, true)) {
